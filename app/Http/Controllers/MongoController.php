d2868c055529a0c6fca6eefaa67521df7d391559
sam <autumnattack@gmail.com>
2020-04-14 16:53:25 +0800

    OK!

--- app/Http/Controllers/MongoController.php
+++ app/Http/Controllers/MongoController.php
@@ -26,18 +26,6 @@ class MongoController extends Controller
         $this->userService = new UserService();
     }
 
-    function std_class_object_to_array($stdclassobject)
-    {
-        $array = [];
-        $_array = is_object($stdclassobject) ? get_object_vars($stdclassobject) : $stdclassobject;
-        foreach ($_array as $key => $value) {
-            $value = (is_array($value) || is_object($value)) ? std_class_object_to_array($value) : $value;
-            $array[$key] = $value;
-        }
-
-        return $array;
-    }
-
     public function index(Request $request)
     {
         $userEntity = Mapper::map($request->getContent(), new UserEntity);
